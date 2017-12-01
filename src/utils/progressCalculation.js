258e19b1c3b825d457903126ada1ada0ecdf1fa4
German Topalov <gaijin773@yandex.ru>
2017-12-01 22:14:49 +0700

    [update] mobile menu added

--- src/utils/progressCalculation.js
+++ src/utils/progressCalculation.js
@@ -1,7 +1,7 @@
 export default function () {
   setTimeout(() => {
     const els = document.getElementsByClassName('progress');
-    els[0].style.height = 0;
+    els[0].style.height = '0';
     setTimeout(() => {
       els[0].style.display = 'none';
     }, 500)
