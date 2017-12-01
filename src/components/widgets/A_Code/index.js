b9ee83644dc26c7c5de3fbd52729d2719e5f2fe2
Kherel Kechil <kherel@gmail.com>
2017-12-01 15:26:57 +0800

    [fix]

--- src/components/widgets/A_Code/index.js
+++ src/components/widgets/A_Code/index.js
@@ -1,34 +1,31 @@
 import React from 'react'
-import {prettyPrintOne} from 'code-prettify'
-import * as T from "prop-types"
-import './styles.scss';
-import {cssClassName} from 'utils'
+import { prettyPrintOne } from 'code-prettify'
+import * as T from 'prop-types'
+import './styles.scss'
+import { cssClassName } from 'utils'
 const cn = cssClassName('A_Code')
 
-
-const A_Code = ({mix, codeString, language, linenums}) => {
+const A_Code = ({ mix, codeString, language, linenums }) => {
+  if (__SERVER__) {
+    return null
+  }
 
   const codeData = prettyPrintOne(codeString, language, linenums)
 
-  return (
-    <code
-      className={cn([mix])}
-      dangerouslySetInnerHTML={{__html: codeData}}
-    />
-  )
+  return <code className={cn([mix])} dangerouslySetInnerHTML={{ __html: codeData }} />
 }
-
 
 A_Code.propTypes = {
   mix: T.string, //BEM mixin from parent block
   codeSting: T.string.isRequired, //code string for snippet
   language: T.string, //set code language manually, default: automatic detection
   linenums: T.bool, // sets string numeration, default: true
-};
+}
 
 A_Code.defaultProps = {
   language: null,
   linenums: true,
+  codeString: '',
 }
 
-export default A_Code;
+export default A_Code
