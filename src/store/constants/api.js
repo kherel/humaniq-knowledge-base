c071a420c7b22c75f0aaa57a0dd1945a3e430395
Kherel Kechil <kherel@gmail.com>
2017-12-07 22:38:11 +0800

    [fix]

--- src/store/constants/api.js
+++ src/store/constants/api.js
@@ -1,5 +1,5 @@
 // export const HOST = __DEVELOPMENT__ ? 'http://localhost:8080' : ''
 
-export const DOMAIN_NAME = __DEVELOPMENT__ ? 'localhost:8080' : process.env.DOMEN_URL
+export const DOMAIN_NAME = process.env.DOMEN_URL
 
 export const POSTMAN_ENDPOINT = `http://${DOMAIN_NAME}/postman-api`
