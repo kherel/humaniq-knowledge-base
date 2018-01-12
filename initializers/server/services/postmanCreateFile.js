a8fdc515b0e1521ea6f3c4660160e9045a4b1d8c
Niten <niten2@yandex.ru>
2018-01-12 22:50:24 +0700

    refactoring

--- initializers/server/services/postmanCreateFile.js
+++ initializers/server/services/postmanCreateFile.js
@@ -1,9 +1,13 @@
 import { write } from "initializers/server/services/postman"
 
 const run = async () => {
-  console.log("write postman response in json")
-  await write()
-  process.exit()
+  try {
+    console.log("write postman response in json")
+    await write()
+    process.exit()
+  } catch (err) {
+    console.log(err.stack)
+  }
 }
 
 run()
